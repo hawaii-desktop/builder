4b771cd398cfc06464fcf1ac53ecfc59ee03c8c1
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-06-07 11:57:37 +0200

    Change license to GPL 3 with exceptions

--- lib/hawaiibuildbot/fedora/shell.py
+++ lib/hawaiibuildbot/fedora/shell.py
@@ -3,10 +3,21 @@
 #
 # Copyright (C) 2015 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
 #
-# This program is free software: you can redistribute it and/or modify
+# Author(s):
+#    Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+#
+# $BEGIN_LICENSE:GPL3$
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 2 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation; version 3 (with exceptions) or any
+# later version accepted by Pier Luigi Fiorini, which shall act as a
+# proxy defined in Section 14 of version 3 of the license.
+#
+# Exceptions are described in Hawaii GPL Exception version 1.0,
+# included in the file GPL3_EXCEPTION.txt in this package.
+#
+# Any modifications to this file must keep this entire header intact.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,6 +26,8 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# $END_LICENSE$
 #
 
 from buildbot.plugins import steps
