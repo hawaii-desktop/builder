eca63ffa102b941a609e88c0cf6de5b75aee8140
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-10-18 19:11:43 +0200

    Add Godeps

---
+++ Godeps/_workspace/src/github.com/plimble/sessions/store/filesystem/store.go
@@ -0,0 +1,73 @@
+package filesystem
+
+import (
+	"bytes"
+	"github.com/plimble/sessions"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+var fileMutex sync.RWMutex
+
+type FileSystemStore struct {
+	path string
+}
+
+func NewFileSystemStore(path string) *FileSystemStore {
+	if path == "" {
+		path = os.TempDir()
+	}
+
+	return &FileSystemStore{
+		path: path,
+	}
+}
+
+func (s *FileSystemStore) Get(id string, buf *bytes.Buffer) error {
+	fileMutex.RLock()
+	defer fileMutex.RUnlock()
+
+	f, err := os.OpenFile(filepath.Join(s.path, "session_"+id), os.O_RDONLY, 0755)
+	if err != nil {
+		return err
+	}
+
+	_, err = buf.ReadFrom(f)
+
+	return err
+}
+
+func (s *FileSystemStore) Delete(session *sessions.Session, w http.ResponseWriter) error {
+	fileMutex.Lock()
+	defer fileMutex.Unlock()
+
+	if err := os.Remove(filepath.Join(s.path, "session_"+id)); err != nil {
+		return err
+	}
+
+	session.Options.MaxAge = -1
+	http.SetCookie(w, sessions.NewCookie(session.Name(), session.ID, session.Options))
+	return nil
+}
+
+func (s *FileSystemStore) Save(session *sessions.Session, buf *bytes.Buffer, w http.ResponseWriter) error {
+	fileMutex.Lock()
+	defer fileMutex.Unlock()
+
+	f, err := os.OpenFile(filepath.Join(s.path, "session_"+session.ID), os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0755)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(f, buf)
+	if err != nil {
+		return err
+	}
+
+	http.SetCookie(w, sessions.NewCookie(session.Name(), session.ID, session.Options))
+
+	return nil
+}
